caa6c5806caab9b0f4df1f7b2d3b8c32360769c4
hoàng tân <hoangnhattan2k2@gmail.com>
2022-10-14 10:36:34 +0700

    tan make model

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/cahoc', [CahocController::class, 'ShowAllList']);
+// khóa học
+
+
+// vai trò
