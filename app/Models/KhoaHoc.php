683024625d75372b04659af56ccb2651cb9050d2
NguyenTrungTung <tung9122002@gmail.com>
2022-11-07 22:23:50 +0700

    tung fix model

--- app/Models/KhoaHoc.php
+++ app/Models/KhoaHoc.php
@@ -121,6 +121,7 @@ class KhoaHoc extends Model
             $query = $query->where('ten_khoa_hoc', 'like', '%' . $key . '%');
         }
         // dd($query);
+    }
 
 
     public function remoAll($params = null, $id_danhmuc = null)
