61365352f8fc9cd75c4e464e79bf7a3da7dd74c6
hoàng tân <hoangnhattan2k2@gmail.com>
2022-11-13 13:21:54 +0700

    feature/tan/modul thanh toán

---
+++ app/Http/Controllers/Admin/ThanhToanController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\PhuongThucThanhToan;
+use App\Models\ThanhToan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ThanhToanController extends Controller
+{
+
+    protected $v, $thanhtoan, $phuongthucthanhtoan;
+    public function __construct()
+    {
+        $this->v = [];
+        $this->thanhtoan = new ThanhToan();
+        $this->phuongthucthanhtoan = new PhuongThucThanhToan();
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $this->v['params'] = $request->all();
+        $this->v['list'] = $this->thanhtoan->index($this->v['params'], true, 10);
+        $this->v['phuongthucthanhtoan'] = $this->phuongthucthanhtoan->index(null, false, null);
+        // dd($this->v['list']);
+        return view('admin.thanhtoan.index', $this->v);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->v['phuongthucthanhtoan']  = $this->phuongthucthanhtoan->index(null, false, null);
+
+        if ($request->isMethod("POST")) {
+            $params = [];
+            $params['cols'] = array_map(function ($item) {
+                if ($item == '') {
+                    $item   = null;
+                }
+
+                if (is_string($item)) {
+                    $item = trim($item);
+                }
+
+                return $item;
+            }, $request->post());
+
+            unset($params['cols']['_token']);
+            $res = $this->thanhtoan->create($params);
+            if ($res > 0) {
+                Session::flash('success', 'Thêm thành công');
+                return redirect()->route('route_BE_Admin_List_Thanh_Toan');
+            } else {
+                Session::flash('error', 'Thêm thành công');
+                return redirect()->route('route_BE_Admin_List_Thanh_Toan');
+            }
+        }
+        return view('admin.thanhtoan.add', $this->v);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id, Request $request)
+    {
+        if ($id) {
+            $request->session()->put('id', $id);
+            $this->v['phuongthucthanhtoan'] = $this->phuongthucthanhtoan->index(null, false, null);
+
+            $res = $this->thanhtoan->show($id);
+            // dd($res);
+            if ($res) {
+              
+                $this->v['res'] = $res;
+                return view('admin.thanhtoan.update' , $this->v);
+            } else {
+                Session::flash('error', "Lỗi hiển thị");
+                return back();
+            }
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if ($id) {
+            $res  = $this->thanhtoan->remove($id);
+            if ($res) {
+                Session::flash('success', "Xóa thành công");
+                return back();
+            } else {
+                Session::flash('error', "Xóa thành công");
+                return back();
+            }
+        }
+    }
+
+
+    public function destroyAll(Request $request){
+        // dd($request->all);
+        // $request  =  $request->all();
+        $this->authorize(mb_strtoupper('xóa thanh toán') );
+
+        if($request->isMethod('POST')){
+            $params = [];
+            $params['cols'] = array_map(function($item){
+                return $item;
+            } , $request->all());
+            unset($params['_token']);
+            $res = $this->thanhtoan->remoAll($params);
+            // dd($res);
+
+            if($res > 0){
+                Session::flash('success , "Xóa thành công');
+                return back();
+            }else {
+                Session::flash('error , "Xóa thành công');
+                return back();
+            }
+          
+        }
+    }
+}
---
+++ resources/views/admin/thanhtoan/update.blade.php
@@ -0,0 +1,73 @@
+@extends('Admin.templates.layout')
+@section('content')
+    {{-- hiển thị massage đc gắn ở session::flash('error') --}}
+    @if (Session::has('error'))
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <strong>{{ Session::get('error') }}</strong>
+        </div>
+    @endif
+
+
+    {{-- hiển thị message đc gắn ở session::flash('success') --}}
+
+    @if (Session::has('success'))
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <strong>{{ Session::get('success') }}</strong>
+        </div>
+    @endif
+    <form class="p-5" action=" {{ route('route_BE_Admin_Update_Thanh_Toan') }}" method="post" enctype="multipart/form-data">
+        <div class="row">
+            @csrf
+            <div class="col-6">
+
+                <div class="mb-3">
+                    <label for="chuyenBay" class="form-label">Ngày thanh toán</label>
+                    <input value="{{ old('ngay_thanh_toan') ?? (request()->ngay_thanh_toan ?? $res->ngay_thanh_toan) }}"
+                        type="date" name="ngay_thanh_toan" class="form-control" id=""
+                        aria-describedby="emailHelp">
+                    {{-- hiển thị lỗi validate -  funciton message trong file DanhMucRequest --}}
+                    @error('ngay_thanh_toan')
+                        <span style="color: red"> {{ $message }} </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="chuyenBay" class="form-label">Phương thức thanh toán</label>
+                    <select class="form-control" name="id_phuong_thuc_thanh_toan" id="">
+                        @foreach ($phuongthucthanhtoan as $item)
+                            @if ($item->id == $res->id_phuong_thuc_thanh_toan)
+                                <option selected value="{{ $item->id }}">{{ $item->ten }}</option>
+                            @else
+                                <option value="{{ $item->id }}">{{ $item->ten }}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                    @error('id_phuong_thuc_thanh_toan')
+                        <span style="color: red"> {{ $message }} </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="chuyenBay" class="form-label">Giá</label>
+                    <input class="form-control" type="text" name="gia" id="" value="{{ $res->gia }}">
+                    @error('gia')
+                        <span style="color: red"> {{ $message }} </span>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="chuyenBay" class="form-label">Mô tả</label>
+                    <textarea class="form-control" type="text" name="mo_ta" id="">
+                    @error('mo_ta')
+                        <span style="color: red"> {{ $message }} </span>
+                    @enderror
+                </div>
+
+
+            </div>
+
+        </div>
+        <button type="submit" class="btn btn-primary">Thêm</button>
+
+    </form>
+@endsection
