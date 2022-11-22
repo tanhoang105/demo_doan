608d4d4f45a9f060e4a32eced763129a61c6b2ec
hoàng tân <hoangnhattan2k2@gmail.com>
2022-11-22 13:59:39 +0700

    feature/tan/thêm tài khoản học viên bên quản lý admin

--- app/Http/Controllers/Admin/HocVienController.php
+++ app/Http/Controllers/Admin/HocVienController.php
@@ -6,12 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\HocVien;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
 class HocVienController extends Controller
 {
 
-    protected $v, $hocvien , $user ;
+    protected $v, $hocvien, $user;
     public function __construct()
     {
         $this->v  = [];
@@ -23,15 +24,15 @@ class HocVienController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request )
+    public function index(Request $request)
     {
 
-        $this->authorize(mb_strtoupper('xem học viên') );
+        $this->authorize(mb_strtoupper('xem học viên'));
 
         $this->v['params'] = $request->all();
-        $this->v['list'] = $this->hocvien->index($this->v['params'] , true , 10);
+        $this->v['list'] = $this->hocvien->index($this->v['params'], true, 10);
         // dd($this->v['list']);
-        return view('admin.hocvien.index' , $this->v);
+        return view('admin.hocvien.index', $this->v);
     }
 
     /**
@@ -52,7 +53,52 @@ class HocVienController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->v['params'] = $request->all();
+        if ($request->isMethod('POST')) {
+            // thực hiện thêm dữ liệu
+            $params = [];
+            $params['cols'] = array_map(function ($item) {
+                if ($item == '') {
+                    $item = null;
+                }
+
+                if (is_string($item)) {
+                    $item = trim($item);
+                }
+                return $item;
+            }, $request->post());
+            unset($params['cols']['_token']);
+            $params['cols']['vai_tro_id'] = 4;
+            $params['cols']['hinh_anh'] = null;
+            // dd($params);
+            if ($request->file('hinh_anh')) {
+
+                $params['cols']['hinh_anh'] = $this->uploadFile($request->file('hinh_anh'));
+            }
+            //            dd($params['cols']);
+            $res = $this->user->create($params);
+
+            if ($res > 0) {
+
+                $this->hocvien->insert([
+                    'user_id' => $res,
+                    'ten_hoc_vien' => $params['cols']['name'],
+                    'dia_chi' => $params['cols']['dia_chi'],
+                    'email' => $params['cols']['email'],
+                    'sdt' => $params['cols']['sdt'],
+                    'hinh_anh' => $params['cols']['hinh_anh'],
+                ]);
+
+
+
+                Session::flash('success', 'Thêm thành công');
+                return redirect()->route('route_BE_Admin_List_Hoc_Vien');
+            } else {
+                Session::flash('error', 'Thêm không thành công');
+                return redirect()->route('route_BE_Admin_List_Hoc_Vien');
+            }
+        }
+        return view('admin.hocvien.add', $this->v);
     }
 
     /**
@@ -72,9 +118,17 @@ class HocVienController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id, Request $request)
     {
-        //
+        if ($id) {
+            $request->session()->put('id', $id);
+            $res  =  $this->hocvien->show($id);
+            $this->v['res'] = $res;
+            return view('admin.hocvien.update', $this->v);
+        } else {
+            Session::flash('error', "Lỗi không tìm đc bản ghi");
+            return back();
+        }
     }
 
     /**
@@ -84,9 +138,56 @@ class HocVienController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+
+        if (session('id')) {
+            $id = session('id');
+            $params = [];
+            $params['cols'] = array_map(function ($item) {
+                if ($item == '') {
+                    $item = null;
+                }
+                if (is_string($item)) {
+                    $item = trim($item);
+                }
+                return $item;
+            }, $request->post());
+            //            dd($id);
+            unset($params['cols']['_token']);
+            $params['cols']['id'] = $id;
+            $params['cols']['hinh_anh'] = null;
+            if ($request->file('hinh_anh')) {
+
+                $params['cols']['hinh_anh'] = $this->uploadFile($request->file('hinh_anh'));
+            }
+
+            if ($request->input('password')) {
+                $params['cols']['password'] = Hash::make($params['cols']['password']);
+            } else {
+                unset($params['cols']['password']);
+            }
+
+            //    dd($params['cols']);
+            $res = $this->user->saveupdate($params);
+
+            if ($res > 0) {
+                HocVien::where('user_id', $id)->update([
+                    'user_id' => $id,
+                    'ten_hoc_vien' => $params['cols']['name'],
+                    'dia_chi' => $params['cols']['dia_chi'],
+                    'email' => $params['cols']['email'],
+                    'sdt' => $params['cols']['sdt'],
+                    'hinh_anh' => $params['cols']['hinh_anh'],
+                ]);
+            
+                Session::flash('success', 'Cập nhập thành công');
+                return redirect()->route('route_BE_Admin_List_Hoc_Vien');
+            } else {
+                Session::flash('error', 'Cập nhập không thành công');
+                return redirect()->route('route_BE_Admin_List_Hoc_Vien');
+            }
+        }
     }
 
     /**
@@ -98,53 +199,51 @@ class HocVienController extends Controller
     public function destroy($id)
     // id học viên xóa
     {
-        $this->authorize(mb_strtoupper('xóa học viên') );
+        $this->authorize(mb_strtoupper('xóa học viên'));
 
         // xóa học viên trong bảng học viên thì xóa luôn trong bảng user
-        if($id){
-            
+        if ($id) {
+
             $res = $this->hocvien->remove($id);
-          
+
             // dd($id_user);
             // xóa học viên xóa => users
-            if($res > 0){
+            if ($res > 0) {
                 $this->user->remove($id);
-                Session::flash('success' , 'Xóa thành công ');
+                Session::flash('success', 'Xóa thành công ');
                 return back();
-            }else {
-                Session::flash('error' , 'Xóa không thành công ');
+            } else {
+                Session::flash('error', 'Xóa không thành công ');
                 return back();
             }
         }
     }
 
 
-    public function destroyAll(Request $request){
+    public function destroyAll(Request $request)
+    {
         // dd($request->all);
         // $request  =  $request->all();
-        $this->authorize(mb_strtoupper('xóa học viên') );
+        $this->authorize(mb_strtoupper('xóa học viên'));
 
-        if($request->isMethod('POST')){
+        if ($request->isMethod('POST')) {
             $params = [];
-            $params['cols'] = array_map(function($item){
+            $params['cols'] = array_map(function ($item) {
                 return $item;
-            } , $request->all());
+            }, $request->all());
             unset($params['_token']);
             $res = $this->hocvien->remoAll($params);
             // dd($res);
 
-            if($res > 0){
+            if ($res > 0) {
                 // khi xóa thành công những học viên này thì cần xóa những user có id tương ứng với user_id học viên
                 $this->user->remoAll($params);
                 Session::flash('success , "Xóa thành công');
                 return back();
-            }else {
+            } else {
                 Session::flash('error , "Xóa thành công');
                 return back();
             }
-          
         }
     }
-
-   
 }
--- resources/views/admin/hocvien/index.blade.php
+++ resources/views/admin/hocvien/index.blade.php
@@ -1,7 +1,7 @@
 @extends('Admin.templates.layout')
 @section('content')
     <div class="row p-3">
-        <a style="color: red" href=" {{ route('route_BE_Admin_Add_Khuyen_Mai') }}">
+        <a style="color: red" href=" {{ route('route_BE_Admin_Add_Hoc_Vien') }}">
             <button class='btn btn-primary'> <i class="fas fa-plus "></i> Thêm</button>
         </a>
 
@@ -22,53 +22,56 @@
     @endif
     <form method="post" action="{{ route('route_BE_Admin_Xoa_All_Hoc_Vien') }}" enctype="multipart/form-data">
         @csrf
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-
-                  <th> <input id="check_all" type="checkbox" /></th>
-                <th scope="col">STT</th>
-                <th scope="col">Tên học viên </th>
-                <th scope="col">Địa chỉ </th>
-                <th scope="col">Email </th>
-                <th scope="col">Số điện thoại</th>
-                <th scope="col">Sửa</th>
-                <th scope="col">
-                    <button class="btn btn-default" type="submit" class="btn" style="">Xóa</button>
-
-                  </th>
-
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($list as $key => $item)
+        <table class="table table-bordered">
+            <thead>
                 <tr>
-                    <td><input class="checkitem" type="checkbox" name="id[]" value="{{ $item->user_id}}" /></td>
-                    <th scope="row"> {{ $loop->iteration }}</th>
-                    <td> {{ $item->ten_hoc_vien }}</td>
-                    <td> {{ $item->dia_chi }}</td>
-                    <td> {{ $item->email }}</td>
-                    <td>
-                        {{ $item->sdt }}
-                    </td>
 
-                    <td>
-                        <a href="{{ route('route_BE_Admin_Edit_Hoc_Vien', ['id' => $item->id]) }}">
-                            <button class="btn btn-success">
-                            <i class="fas fa-edit "></i> Sửa</button></a>
-                    </td>
-                    <td>
-                        <a href="{{ route('route_BE_Admin_Xoa_Hoc_Vien', ['id' => $item->user_id]) }}">
-                            <button onclick="return confirm('Bạn có chắc muốn xóa ?')" class="btn btn-danger">
-                            <i class="fas fa-trash-alt"></i> Xóa</button></a>
-                    </td>
+                    <th> <input id="check_all" type="checkbox" /></th>
+                    <th scope="col">STT</th>
+                    <th scope="col">Tên học viên </th>
+                    <th scope="col">Địa chỉ </th>
+                    <th scope="col">Email </th>
+                    <th scope="col">Số điện thoại</th>
+                    <th scope="col">Sửa</th>
+                    <th scope="col">
+                        <button class="btn btn-default" type="submit" class="btn" style="">Xóa</button>
+
+                    </th>
 
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($list as $key => $item)
+                    <tr>
+                        <td><input class="checkitem" type="checkbox" name="id[]" value="{{ $item->user_id }}" /></td>
+                        <th scope="row"> {{ $loop->iteration }}</th>
+                        <td> {{ $item->ten_hoc_vien }}</td>
+                        <td> {{ $item->dia_chi }}</td>
+                        <td> {{ $item->email }}</td>
+                        <td>
+                            {{ $item->sdt }}
+                        </td>
 
-        </tbody>
-    </table>
-</form>
+                        <td>
+                            <button class="btn btn-success">
+                                <a style="color: #fff" href="{{ route('route_BE_Admin_Edit_Hoc_Vien', ['id' => $item->user_id]) }}">
+                                    <i class="fas fa-edit "></i> Sửa</a>
+                            </button>
+                        </td>
+                        <td>
+                            <button onclick="return confirm('Bạn có chắc muốn xóa ?')" class="btn btn-danger">
+                                <a style="color: #fff" href="{{ route('route_BE_Admin_Xoa_Hoc_Vien', ['id' => $item->user_id]) }}">
+                                    <i class="fas fa-trash-alt"></i> Xóa</a>
+
+                            </button>
+                        </td>
+
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </form>
     <div class="">
         <div class="d-flex align-items-center justify-content-between flex-wrap">
             {{ $list->appends('params')->links() }}
