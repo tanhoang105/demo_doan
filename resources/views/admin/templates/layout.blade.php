0a1919c15003514f78f51f9648b62122674b504c
hoàng tân <hoangnhattan2k2@gmail.com>
2022-12-18 01:10:58 +0700

    tan commit sidebar

--- resources/views/admin/templates/layout.blade.php
+++ resources/views/admin/templates/layout.blade.php
@@ -227,8 +227,25 @@
     })
 </script>
 
-g
+<script>
+   
+    /*** add active class and stay opened when selected ***/
+    var url = window.location;
 
+    // // for sidebar menu entirely but not cover treeview
+    $('li.nav-item a').filter(function() {
+        if (this.href) {
+            return this.href == url || url.href.indexOf(this.href) == 0;
+        }
+    }).addClass('active');
+
+    // for the treeview
+    $('ul.nav-treeview a').filter(function() {
+        if (this.href) {
+            return this.href == url || url.href.indexOf(this.href) == 0;
+        }
+    }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+</script>
 
 
 </html>
